65d433859c05aea2050c05fdd434ed6be63f38eb
53142 <89957019+53142@users.noreply.github.com>
2024-07-24 12:21:02 -0700

    Add project files.

---
+++ Encryption-Program/Encryption-Program.cpp
@@ -0,0 +1,12 @@
+﻿// Encryption-Program.cpp : Defines the entry point for the application.
+//
+
+#include "Encryption-Program.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
